7287de0594354fb13f5b3e96b0d747efbf13fca0
Manoela Portes <129810482+ManoelaPortesAlves@users.noreply.github.com>
2023-04-24 11:53:03 -0300

    first commit

---
+++ src/data/questions.js
@@ -0,0 +1,74 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      category: "Corpo Humano",
+      type: "multiple",
+      difficulty: "easy",
+      question: "É com essa parte do corpo que nós conseguimos ir pra lá e pra cá. Dá pra esticar, dobrar e até cruzar. Qual parte do corpo é essa?",
+      img: "src/img/perna.jpg",
+      answer: "Perna",
+      options: [
+        "Braço",
+        "Perna",
+        "Pescoço",
+      ],
+    },
+     {
+      numb: 2,
+      category: "Corpo Humano",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Nessa parte do corpo é legal de ver como somos diferentes. Nela, carregamos o nosso cabelo do jeitinho que ele é, nossos olhos de todas as cores, além da orelha e do nariz que é só nosso. Qual parte do corpo é essa?",
+      img: "src/img/cabeca.jpg",
+      answer: "Cabeça",
+      options: [
+        "Joelho",
+        "Mão",
+        "Cabeça",
+      ],
+    },
+    {
+      numb: 3,
+      category: "Corpo Humano",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Essa é a parte do corpo que se dobra para a gente dar um abraço apertado e é ela que liga nossa mão ao restinho do braço. Qual parte do corpo é essa?",
+      img: "src/img/cotovelo.jpg",
+      answer: "Cotovelo",
+      options: [
+        "Cabeça",
+        "Cotovelo",
+        "Pescoço",
+      ],
+    },
+    {
+      numb: 4,
+      category: "Corpo Humano",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Pode ficar aberta ou fechada, pode dar oi e dar tchau. Com ela podemos escrever, segurar um objeto e até fazer carinho no nosso pet. Qual parte do corpo é essa?",
+      img: "src/img/mao.jpg",
+      answer: "Mão",
+      options: [
+        "Pescoço",
+        "Orelha",
+        "Mão",
+      ],
+    },
+    {
+      numb: 5,
+      category: "Corpo Humano",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Essa é a parte que liga os nossos braços ao tronco. Por causa dela podemos mexer os braços pra todo lado na hora de dançar e até carregar a mochila na hora de ir pra escola. Qual parte do corpo é essa?",
+      img: "src/img/ombro.jpg",
+      answer: "Ombro",
+      options: [
+        "Cabeça",
+        "Ombro",
+        "Pescoço",
+      ],
+    },
+  ];
+  
